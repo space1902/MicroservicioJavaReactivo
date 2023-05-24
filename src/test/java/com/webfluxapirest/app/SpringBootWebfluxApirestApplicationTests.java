017ad9eba02693e9096024551494bfcf2b8a6100
space1902 <victorup1902@gmail.com>
2023-05-24 10:34:28 -0500

    upload config eureka server

--- src/test/java/com/webfluxapirest/app/SpringBootWebfluxApirestApplicationTests.java
+++ src/test/java/com/webfluxapirest/app/SpringBootWebfluxApirestApplicationTests.java
@@ -1,13 +1,62 @@
 package com.webfluxapirest.app;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.util.Collections;
+import java.util.List;
 
-@SpringBootTest
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import com.webfluxapirest.app.models.Producto;
+import com.webfluxapirest.app.service.ProductoService;
+
+import reactor.core.publisher.Mono;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SpringBootWebfluxApirestApplicationTests {
 
+	@Autowired
+	private WebTestClient client;
+	
+	@Autowired
+	private ProductoService service;
+	
 	@Test
-	void contextLoads() {
+	void listarTest() {
+		client.get()
+		.uri("/api/v2/productos")
+		.accept(MediaType.APPLICATION_JSON_UTF8)
+		.exchange()
+		.expectStatus().isOk()
+		.expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+		.expectBodyList(Producto.class)
+		.consumeWith(resp -> {
+			List<Producto> productos = resp.getResponseBody();
+			productos.forEach( p -> {
+				System.out.println(p.getNombre());
+			});
+
+			Assertions.assertThat(productos.size()>0).isTrue();
+		});
+		
+		//.hasSize(7);
+	}
+	
+	@Test
+	void verTest() {
+		Mono<Producto> producto = null;
+		client.get()
+		.uri("/api/v2/productos/{id}", Collections.singletonMap("id", ""))
+		.accept(MediaType.APPLICATION_JSON_UTF8)
+		.exchange()
+		.expectStatus().isOk()
+		.expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8);
 	}
 
 }
